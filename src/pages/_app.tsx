95055dc6daf36d5a11f09b88f2ae6ef6aac5a4fe
Akshit1311 <a4748g@gmail.com>
2023-04-20 17:01:18 +0530

    add: video sharing capability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { initialize } from "next/dist/server/lib/render-server";
+import { useEffect } from "react";
+
+import { useHuddle01 } from "@huddle01/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { initialize } = useHuddle01();
+
+  useEffect(() => {
+    initialize(process.env.NEXT_PUBLIC_PROJECT_ID as string);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
