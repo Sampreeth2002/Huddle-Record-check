eb2aa855b4a3673b16845969892168a7385ff1b0
Akshit1311 <a4748g@gmail.com>
2023-04-20 19:37:02 +0530

    add: deal creation and recording

--- src/pages/[roomId]/index.tsx
+++ src/pages/[roomId]/index.tsx
@@ -15,6 +15,11 @@ import {
   useMeetingMachine,
 } from "@huddle01/react/hooks";
 
+// fvm
+import { ethers } from "ethers";
+import contract from "@/contracts/DealClient.json";
+const CID = require("cids");
+
 const Room = () => {
   // roomId logic
   const [roomId, setRoomId] = useState("");
@@ -24,12 +29,38 @@ const Room = () => {
   // refs
   const videoRef = useRef<HTMLVideoElement>(null);
 
+  // fvm
+  const [errorMessageSubmit, setErrorMessageSubmit] = useState("");
+  const contractAddress =
+    process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
+    "0x08661959fdFFf3cB4C40DC75c10eb561c6642Ea6";
+  const contractABI = contract.abi;
+  const [commP, setCommP] = useState(
+    "baga6ea4seaqkp2pjlh6avlvee6ib2maanav5sc35l5glf3zm6rd6hmfgcx5xeji"
+  );
+  const [carLink, setCarLink] = useState(
+    "https://data-depot.lighthouse.storage/api/download/download_car?fileId=862fb115-d24a-4ff1-a1c8-eadbbbfd19cf.car"
+  );
+  const [pieceSize, setPieceSize] = useState("32768");
+  const [carSize, setCarSize] = useState("18445");
+  const [proposingDeal, setProposingDeal] = useState(false);
+  const [txSubmitted, setTxSubmitted] = useState("");
+  const [network, setNetwork] = useState("");
+  const [dealID, setDealID] = useState("");
+  const dealClient = useRef<ethers.Contract | null>(null);
+
   // huddle01
   const { state } = useMeetingMachine();
   const { stream: camStream, fetchVideoStream, produceVideo } = useVideo();
   const { joinLobby, isLobbyJoined } = useLobby();
   const { joinRoom } = useRoom();
-  const { startRecording, inProgress } = useRecording();
+  const {
+    startRecording,
+    stopRecording,
+    error,
+    data: recordingData,
+    inProgress,
+  } = useRecording();
   const { peers } = usePeers();
 
   useEffect(() => {
@@ -44,6 +75,126 @@ const Room = () => {
   useEventListener("lobby:cam-on", () => {
     if (camStream && videoRef.current) videoRef.current.srcObject = camStream;
   });
+
+  const handleSubmit = async () => {
+    // This will be handling deal proposal submission sometime in the future.
+    // do something with the carLink value, like send it to a backend API
+    console.log(commP);
+    console.log(carLink);
+    console.log(pieceSize);
+    console.log(carSize);
+
+    try {
+      setErrorMessageSubmit("");
+      const cid = new CID(commP);
+      const { ethereum } = window as any;
+      if (ethereum) {
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = await provider.getSigner();
+        dealClient.current = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const extraParamsV1 = [
+          carLink,
+          carSize,
+          false, // taskArgs.skipIpniAnnounce,
+          false, // taskArgs.removeUnsealedCopy
+        ];
+        const DealRequestStruct = [
+          cid.bytes, //cidHex
+          pieceSize, //taskArgs.pieceSize,
+          false, //taskArgs.verifiedDeal,
+          commP, //taskArgs.label,
+          520000, // startEpoch
+          1555200, // endEpoch
+          0, // taskArgs.storagePricePerEpoch,
+          0, // taskArgs.providerCollateral,
+          0, // taskArgs.clientCollateral,
+          1, //taskArgs.extraParamsVersion,
+          extraParamsV1,
+        ];
+        // console.log(await provider.getBalance("0x42c930a33280a7218bc924732d67dd84d6247af4"));
+        console.log(dealClient.current.interface);
+        const transaction = await dealClient.current.makeDealProposal(
+          DealRequestStruct
+        );
+        console.log("Proposing deal...");
+        setProposingDeal(true);
+        const receipt = await transaction.wait();
+        console.log(receipt);
+        setProposingDeal(false);
+        setTxSubmitted("Transaction submitted! " + receipt.hash);
+
+        dealClient.current.on(
+          "DealProposalCreate",
+          (id, size, verified, price) => {
+            console.log(id, size, verified, price);
+          }
+        );
+
+        console.log("Deal proposed! CID: " + cid);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error: any) {
+      console.log({ error });
+      setErrorMessageSubmit(
+        "Something went wrong. " + error.name + " " + error.message
+      );
+      return;
+    }
+  };
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window as any;
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+    const provider = new ethers.BrowserProvider(ethereum);
+    const network = await provider.getNetwork();
+    setNetwork(network.chainId as any);
+    console.log(network.chainId);
+
+    ethereum.request({ method: "eth_accounts" }).then((accounts: any) => {
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an account:", account);
+      } else {
+        console.log("No account found");
+      }
+    });
+  };
+
+  const dealIDHandler = async () => {
+    setDealID("Waiting for acceptance by SP...");
+    const cid = new CID(commP);
+    var refresh = setInterval(async () => {
+      console.log(cid.bytes);
+      if (cid === undefined) {
+        setDealID("Error: CID not found");
+        clearInterval(refresh);
+      }
+      console.log("Checking for deal ID...");
+      const dealID = await dealClient.current?.pieceDeals(cid.bytes);
+      console.log(dealID);
+      if (dealID && dealID !== "0") {
+        // If your deal has already been submitted, you can get the deal ID by going to https://hyperspace.filfox.info/en/deal/<dealID>
+        // The link will show up in the frontend: once a deal has been submitted, its deal ID stays constant. It will always have the same deal ID.
+        setDealID("https://hyperspace.filfox.info/en/deal/" + dealID);
+        clearInterval(refresh);
+      }
+    }, 5000);
+  };
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -141,13 +292,32 @@ const Room = () => {
         <div>
           <Button
             disabled={!startRecording.isCallable}
-            onClick={() => startRecording(roomId)}
+            onClick={() =>
+              startRecording(`${window.location.href}rec/${roomId}`)
+            }
           >
             startRecording()
           </Button>
-          <div>isLobbyJoined: {isLobbyJoined.toString()}</div>
+          <div>error: {error}</div>
+        </div>
+        <div>
+          <Button disabled={!stopRecording.isCallable} onClick={stopRecording}>
+            stopRecording()
+          </Button>
+          <div>inProgress: {inProgress.toString()}</div>
+        </div>
+
+        <div>
+          <Button onClick={handleSubmit}>Submit Deal</Button>
+        </div>
+        <div>
+          <Button onClick={dealIDHandler}>Get Deal ID</Button>
         </div>
       </div>
+      <div className="text-green-300">
+        Deal Status: {proposingDeal ? "Proposing Deal" : dealID || "Idle"}
+      </div>
+      <div className="text-green-300">{(recordingData as any)?.s3Url}</div>
     </main>
   );
 };
